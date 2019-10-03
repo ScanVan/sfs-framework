1d25a80601ebd7bb8dff16ca92f3373253e51965
Charles Papon <charles.papon.90@gmail.com>
2019-10-03 17:07:06 +0200

    Reduce the number of points from akaze

--- src/framework-sparsefeature.cpp
+++ src/framework-sparsefeature.cpp
@@ -12,7 +12,7 @@ void akazeFeatures(	cv::Mat* image,
 					cv::Mat* desc) {
 
 	// The following parameters worked for the sequence 1-34 of the Sion Outdoor dataset
-	cv::Ptr<cv::AKAZE> akaze = cv::AKAZE::create(cv::AKAZE::DESCRIPTOR_MLDB, 0, 3, 0.00025f, 4, 4, cv::KAZE::DIFF_PM_G2);
+	cv::Ptr<cv::AKAZE> akaze = cv::AKAZE::create(cv::AKAZE::DESCRIPTOR_MLDB, 0, 3, 0.001f, 4, 4, cv::KAZE::DIFF_PM_G2);
 
 	akaze->detectAndCompute(*image, *mask, *keypoints, *desc);
 }
