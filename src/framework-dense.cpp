b85be4cdb4148c5667408b21b42889713f8ec138
Charles Papon <charles.papon.90@gmail.com>
2019-11-28 13:59:01 +0100

    Add dense executable

---
+++ src/framework-dense.cpp
@@ -0,0 +1,30 @@
+/*
+ *  sfs-framework
+ *
+ *      Nils Hamel - nils.hamel@bluewin.ch
+ *      Charles Papon - charles.papon.90@gmail.com
+ *      Copyright (c) 2019 DHLAB, EPFL & HES-SO Valais-Wallis
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "framework.hpp"
+
+int main(int argc, char *argv[]){
+    std::cout << "Hello dense world!" << std::endl;
+
+    return 0;
+
+}
+
